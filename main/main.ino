f70f8e786b71f821e13874d8d155d4acd2c57de4
hyu nani <60097661+hyu-nani@users.noreply.github.com>
2021-02-27 14:11:58 +0900

    update

--- main/main.ino
+++ main/main.ino
@@ -67,7 +67,7 @@ void loop() {
     LCD_smooth_on(3,backlight);
     while(1){
       print_display(display_x,display_y);
-      delay(10);
+      delay(100);
       data = swcheck();
       if(data == 'D'){
         display_set_bio();
@@ -80,9 +80,9 @@ void loop() {
         break;
       }
       else if (data == 'U'){
-        display_set_setting();
+        display_set_bio();
         display_frame();
-        mode = 2;
+        mode = 1;
         for(int i = -pg_change_num ;i<=2 ;i++){
           display_y+=(9*i*i)/400+1;
           print_display(display_x,display_y);
@@ -121,7 +121,7 @@ void loop() {
         }
         display_set_temp();
         display_frame();
-        mode=3;
+        mode=4;
         delay(300);
         for(int i =-2 ;i<=pg_change_num ;i++){
           display_y+=(9*i*i)/400+1;
@@ -141,7 +141,7 @@ void loop() {
   }
   
   //==========================================================
-  while(mode == 2){   //setting
+  while(mode == 2){   //temp
     
     while(1){
       print_display(display_x,display_y);
@@ -189,7 +189,7 @@ void loop() {
   }
   
   //==========================================================
-  while(mode == 3){   //temp
+  while(mode == 3){   //setting
     
     while(1){
       print_display(display_x,display_y);
@@ -202,7 +202,7 @@ void loop() {
         }
         display_set_bio();
         display_frame();
-        mode=1;
+        mode=4;
         delay(300);
         for(int i = -2 ;i<=pg_change_num ;i++){
           display_y+=(9*i*i)/400+1;
@@ -230,11 +230,60 @@ void loop() {
           display_y-=(9*i*i)/400+1;
           print_display(display_x,display_y);
         }
+        mode=5;
+        break;
+      }
+    }
+  }
+  //==========================================================
+  while(mode == 4){   //acc
+    
+    while(1){
+      print_display(display_x,display_y);
+      delay(10);
+      data = swcheck();
+      if(data == 'D'){
+        for(int i = -pg_change_num ;i<=2 ;i++){
+          display_y-=(9*i*i)/400+1;
+          print_display(display_x,display_y);
+        }
+        display_set_bio();
+        display_frame();
+        mode=1;
+        delay(300);
+        for(int i = -2 ;i<=pg_change_num ;i++){
+          display_y+=(9*i*i)/400+1;
+          print_display(display_x,display_y);
+        }
+        break;
+      }
+      else if (data == 'U'){
+        for(int i = -pg_change_num ;i<=2 ;i++){
+          display_y-=(9*i*i)/400+1;
+          print_display(display_x,display_y);
+        }
+        display_set_setting();
+        display_frame();
+        mode=3;
+        delay(300);
+        for(int i =-2 ;i<=pg_change_num ;i++){
+          display_y+=(9*i*i)/400+1;
+          print_display(display_x,display_y);
+        }
+        break;
+      }
+      else if (data == 'M'){
+        for(int i = -pg_change_num ;i<=2 ;i++){
+          display_y-=(9*i*i)/400+1;
+          print_display(display_x,display_y);
+        }
         mode=0;
         break;
       }
     }
   }
+  //================================================================
+  //option
 }
 char swcheck()
 {
