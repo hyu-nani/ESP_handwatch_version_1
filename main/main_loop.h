8c56c484f047de79acd46fd4ad28919d96a8d2d9
hyu nani <60097661+hyu-nani@users.noreply.github.com>
2021-03-07 23:59:35 +0900

    update main_loop.h

--- main/main_loop.h
+++ main/main_loop.h
@@ -8,28 +8,34 @@
 //loop main
 void main_loop(){
 	//========================================================== default clock
-	while(mode == 0){ //clock
+	while(mode == 0)
+	{ //clock
 		Clock_set();
 		print_display(display_x,display_y);
 		LCD_smooth_on(4,backlight);
-		while(1){
+		while(true)
+		{
 			Clock_play();
 			print_display(display_x,display_y);
 			delay(100);
 			data = swcheck();
-			if(data == 'D'){
+			if(data == 'D')
+			{
 				table_set_health();
 				mode=1;
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y+=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				break;
 			}
-			else if (data == 'U'){
+			else if (data == 'U')
+			{
 				table_set_health();
 				mode = 1;
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y+=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
@@ -39,45 +45,55 @@ void main_loop(){
 	}
 	
 	//========================================================== health mode
-	while(mode == 1){   
+	while(mode == 1)
+	{   
 		
-		while(1){
+		while(true)
+		{
 			print_display(display_x,display_y);
 			delay(10);
 			data = swcheck();
-			if(data == 'D'){
+			if(data == 'D')
+			{
 				Clock_set();
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y-=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				table_set_temp();
 				mode=3;
 				delay(300);
-				for(int i = -2 ;i<=pg_change_num ;i++){
+				for(int i = -2 ;i<=pg_change_num ;i++)
+				{
 					display_y+=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				break;
 			}
-			else if (data == 'U'){
+			else if (data == 'U')
+			{
 				Clock_set();
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y-=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				table_set_motion();
 				mode=7;
 				delay(300);
-				for(int i =-2 ;i<=pg_change_num ;i++){
+				for(int i =-2 ;i<=pg_change_num ;i++)
+				{
 					display_y+=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				break;
 			}
-			else if (data == 'M'){
+			else if (data == 'M')
+			{
 				Clock_set();
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y-=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
@@ -92,45 +108,55 @@ void main_loop(){
 	  
 	}
 	//========================================================== temperature
-	while(mode == 3){
+	while(mode == 3)
+	{
 		
-		while(1){
+		while(true)
+		{
 			print_display(display_x,display_y);
 			delay(10);
 			data = swcheck();
-			if(data == 'D'){
+			if(data == 'D')
+			{
 				Clock_set();
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y-=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				table_set_setting();
 				mode=5;
 				delay(300);
-				for(int i = -2 ;i<=pg_change_num ;i++){
+				for(int i = -2 ;i<=pg_change_num ;i++)
+				{
 					display_y+=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				break;
 			}
-			else if (data == 'U'){
+			else if (data == 'U')
+			{
 				Clock_set();
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y-=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				table_set_health();
 				mode=1;
 				delay(300);
-				for(int i =-2 ;i<=pg_change_num ;i++){
+				for(int i =-2 ;i<=pg_change_num ;i++)
+				{
 					display_y+=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				break;
 			}
-			else if (data == 'M'){
+			else if (data == 'M')
+			{
 				Clock_set();
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y-=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
@@ -142,49 +168,59 @@ void main_loop(){
 	//========================================================== into temperature
 	while(mode == 4)
 	{
-	  
+	  while(true)
+	  {
+	    
+	  }
 	}
 	//========================================================== setting pop-up
 	while(mode == 5)
 	{   
-		
-		while(1){
+		while(true)
+		{
 			print_display(display_x,display_y);
 			delay(10);
 			data = swcheck();
 			if(data == 'D'){
 				Clock_set();
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y-=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				table_set_motion();
 				mode=7;
 				delay(300);
-				for(int i = -2 ;i<=pg_change_num ;i++){
+				for(int i = -2 ;i<=pg_change_num ;i++)
+				{
 					display_y+=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				break;
 			}
-			else if (data == 'U'){
+			else if (data == 'U')
+			{
 				Clock_set();
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y-=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				table_set_temp();
 				mode=3;
 				delay(300);
-				for(int i =-2 ;i<=pg_change_num ;i++){
+				for(int i =-2 ;i<=pg_change_num ;i++)
+				{
 					display_y+=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				break;
 			}
-			else if (data == 'M'){
+			else if (data == 'M')
+			{
 				table_setmode();
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y-=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
@@ -201,45 +237,55 @@ void main_loop(){
 		break;
 	}
 	//================================================================ motion mode
-	while(mode == 7){
-		
-		while(1){
+	while(mode == 7)
+	{
+		//initial
+		while(true)
+		{
 			print_display(display_x,display_y);
 			delay(10);
 			data = swcheck();
-			if(data == 'D'){
+			if(data == 'D')
+			{
 				Clock_set();
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y-=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				table_set_health();
 				mode=1;
 				delay(300);
-				for(int i = -2 ;i<=pg_change_num ;i++){
+				for(int i = -2 ;i<=pg_change_num ;i++)
+				{
 					display_y+=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				break;
 			}
-			else if (data == 'U'){
+			else if (data == 'U')
+			{
 				Clock_set();
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y-=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				table_set_setting();
 				mode=5;
 				delay(300);
-				for(int i =-2 ;i<=pg_change_num ;i++){
+				for(int i =-2 ;i<=pg_change_num ;i++)
+				{
 					display_y+=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
 				break;
 			}
-			else if (data == 'M'){
+			else if (data == 'M')
+			{
 				Clock_set();
-				for(int i = -pg_change_num ;i<=2 ;i++){
+				for(int i = -pg_change_num ;i<=2 ;i++)
+				{
 					display_y-=(9*i*i)/400+1;
 					print_display(display_x,display_y);
 				}
