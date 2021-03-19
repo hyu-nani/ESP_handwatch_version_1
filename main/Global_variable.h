1ad98071fb7e4084d25a5b93f29b9d802c1c44a6
hyu nani <60097661+hyu-nani@users.noreply.github.com>
2021-03-20 01:49:16 +0900

    update Global_variable.h

--- main/Global_variable.h
+++ main/Global_variable.h
@@ -17,6 +17,10 @@ bool  connect_wifi = false;
 
 
 //clock
-
+int now_hour=0,now_minute=0,now_second=0,now_day=0,now_year=0;
+int month,weekday;
+int old_hour=0,old_minute=0,old_day=0,old_year=0;
+unsigned long prev_ms=0,get_time_ms=0;
+unsigned long now_ms=millis();
 unsigned long sec_period =			1000;
 unsigned long update_cycle_time =	60000;
