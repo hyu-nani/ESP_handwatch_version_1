9192bfc11e9ffa2bc34313147fdc90d687d2b7a6
hyu nani <60097661+hyu-nani@users.noreply.github.com>
2021-03-20 02:09:21 +0900

    update setting.h

--- main/table/setting.h
+++ main/table/setting.h
@@ -233,28 +233,27 @@ void table_setmode_loop(){ //setting loop
 				table_print(10,10,"=======TIME SET=======",BLACK,1);
 				print_display(display_x,display_y);
 				delay(10);
-				bool  set_time_active = false;
+				bool  set_active = false;
 				cursor_y = 20;
 				while(true)
 				{
+				  TIME_SET:
 					table_fill_block(1,WHITE);
 					table_print(10,cursor_y,"E>",RED,1);
 					table_print(10,10,"=======TIME SET=======",BLUE,1);
 					table_print(20,20,"Summer Time  =",BLACK,1);table_print(100,30,daylightOffset_sec,BLACK,1);table_print(130,30,"ms",BLACK,1);
 					table_print(20,30,"1 Sec period =",BLACK,1);table_print(100,30,sec_period*4,BLACK,1);table_print(130,30,"ms",BLACK,1);
-					table_print(20,40,"GMT Set      =",BLACK,1);table_print(100,30,GMT,BLACK,1);table_print(130,30,"+hour",BLACK,1);
+					table_print(20,40,"+GMT Set     =",BLACK,1);table_print(100,30,GMT,BLACK,1);table_print(130,30,"hour",BLACK,1);
 					table_print(20,50,"CLOCK SET >>",BLACK,1);
 			    table_print(20,60," << BACK Page",BLACK,1);
 			
 					table_set_frame(0,0,160,80,frame_round);
 					print_display(display_x,display_y);
 					data = swcheck();
-					if(data == 'U'){
-						cursor_y+=10;	
-					}
-					else if(data == 'D'){
-						cursor_y-=10;
-					}
+					if(data == 'U'&&set_active == false)
+						cursor_y-=10;	
+					else if(data == 'D'&&set_active == false)
+						cursor_y+=10;
 					else if(data == 'M'&&cursor_y == 60){
 						option_active = false;
 						EEPROM_Data_Save();
@@ -262,26 +261,69 @@ void table_setmode_loop(){ //setting loop
 					}
 					else
 					{
-					  if(cursor_y == 20)
+					  set_active = true;
+					  if(cursor_y == 20)      //SUMMER TIME
 					  {
-					    
+					    if(data == 'U'&&set_active==true)
+					      daylightOffset_sec +=600;
+					    else if(data == 'D'&&set_active==true)
+					      daylightOffset_sec -=600;
+					    else if(data == 'M'&&set_active==true)
+					    {
+					      set_active = false;
+					      goto TIME_SET;
+					    }
 					  }
-					  else if(cursor_y == 30)
+					  else if(cursor_y == 30)//sec_period
 					  {
-					    
+					    if(data == 'U'&&set_active==true)
+					      sec_period += 1;
+					    else if(data == 'D'&&set_active==true)
+					      sec_period -= 1;
+					    else if(data == 'M'&&set_active==true)
+					    {
+					      set_active = false;
+					      goto TIME_SET;
+					    }
+					  }
+					  else if(cursor_y == 40)//GMT
+					  {
+					    if(data == 'U'&&set_active==true)
+					      GMT += 1;
+					    else if(data == 'D'&&set_active==true)
+					      GMT -= 1;
+					    else if(data == 'M'&&set_active==true)
+					    {
+					      set_active = false;
+					      goto TIME_SET;
+					    }
 					  }
 					  else if(cursor_y == 50)
 					  {
-					    
+					    cursor_y =20;
 					    while(true)
 					    {
+					      data = swcheck();
 					      table_fill_block(1,WHITE);
 					      table_print(10,10,"=======CLOCK SET=======",BLUE,1);
 					      table_print(20,20,"hour =",BLACK,1);
 					      table_print(20,30,"Minute =",BLACK,1);
-					      
+					      table_print(20,40,"Second =",BLACK,1);
+					      table_print(20,60," << BACK Page",BLACK,1);
 					      table_set_frame(0,0,160,80,frame_round);
 					      print_display(display_x,display_y);
+					      if(data == 'D')
+					        cursor_y += 10;
+					      else if(data == 'U')
+					        cursor_y -= 10;
+					      else if(data == 'M'&&cursor_y==60)
+					      {
+					        goto TIME_SET;
+					      }
+					      else
+					      {
+					        
+					      }
 					    }
 					  }
 					}
