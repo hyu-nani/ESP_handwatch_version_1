7cf956d4bdef69e86ed3f394bd7daa470cce4137
hyu nani <60097661+hyu-nani@users.noreply.github.com>
2021-03-19 15:43:25 +0900

    update setting.h

--- main/table/setting.h
+++ main/table/setting.h
@@ -230,7 +230,7 @@ void table_setmode_loop(){ //setting loop
 			    table_print(10,10,"=========TIME SET========",BLUE,1);
 			    table_print(10,30,"1 Sec period =",BLACK,1);
 			    table_print(100,30,sec_period,BLACK,1);
-			   
+			    print_display(display_x,display_y);
 			  }
 			}
 			//////////////////////////////////////////////////////////////////////////////////////////////////////
