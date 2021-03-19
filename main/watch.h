8d3c8d1cb61bf99480079f35677eca77ee2bd445
hyu nani <60097661+hyu-nani@users.noreply.github.com>
2021-03-19 15:53:56 +0900

    update watch.h

--- main/watch.h
+++ main/watch.h
@@ -1,24 +1,24 @@
-﻿#define EN2				35 
+#define EN2				    35 
 #define BAT_voltage		34
-#define SD_CS			33
+#define SD_CS			    33
 #define Card_detect		27
 #define wireless			26
-#define sensor_power		25
-#define TFT_DC			15
-#define TFT_RST			14
-#define TFT_CS			32
-#define TFT_LIGHT		16
-#define SW_U				13
-#define SW_M				12
-#define SW_D				5
-#define charge			2
-#define ADXL_CS			4
-#define Power			17
-#define SCK				18
-#define MISO				19
-#define MOSI				23
-//#define SCL				22
-//#define SDA				21
+#define sensor_power	25
+#define TFT_DC			  15
+#define TFT_RST			  14
+#define TFT_CS			  32
+#define TFT_LIGHT	  	16
+#define SW_U			  	13
+#define SW_M			  	12
+#define SW_D			  	5
+#define charge		  	2
+#define ADXL_CS		  	4
+#define Power		     	17
+#define SCK			    	18
+#define MISO			  	19
+#define MOSI			  	23
+//#define SCL			  	22
+//#define SDA			  	21
 
 void watch_pinset(){
 	pinMode(SW_U,INPUT_PULLUP);
