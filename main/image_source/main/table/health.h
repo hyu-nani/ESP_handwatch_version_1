47d23f3da957363d205943da7a8244804322f90c
hyu nani <60097661+hyu-nani@users.noreply.github.com>
2021-05-05 19:01:21 +0900

    update health.h

---
+++ main/image_source/main/table/health.h
@@ -0,0 +1,22 @@
+void table_set_health()
+{
+	//loadImage(SD,load_image, "/Background_image/health_background.c");
+	//loadTableSetBackground(0,80,160,80,load_image);
+	table_image(0,80,160,160,health_image);
+	table_set_frame(0,80,160,80,frame_popup);	
+	Serial.println("Device : set");
+}
+
+현한이 기여워 사랑해 내꼬야 ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ
+현한이에게
+현한아 안녕 나는 너의 여자친구야
+나는 너랑 있어서 엄청행복해
+너 표현못해도 나 좋아해주고 배려해주는 모습 너무 좋아
+너랑 있으면 내 심장이 엄청 두근두근해
+나도 첫 연애라서 잘 모르는 거 많지만 항상 노력할께
+내 남자친구가 되어줘서 고마워
+앞으로도 이쁜 사랑키워가자
+나도 내 할일 열심히 하면서 자랑스러운 여자친구가 될께
+항상 너에게 사랑받고 싶어하는 내 마음 알아주면좋게따
+오빠랑 하고싶은 것도 엄청 많고 매일 보고싶지만 나도 인내심을 길러볼께
+항상 사랑해
